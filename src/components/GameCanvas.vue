dcc575dde0f87046fa3dffed6d8f2fc3265b285d
chenweiyi <737649321@qq.com>
2024-06-15 22:52:20 +0800

    feat: 增加“战报”区域

--- src/components/GameCanvas.vue
+++ src/components/GameCanvas.vue
@@ -9,10 +9,14 @@ import Stage from '../game/Stage.ts'
 import { CANVAS_HEIGHT, CANVAS_WIDTH } from '../game/config'
 
 let stage: Stage
+let img: HTMLImageElement
 const canvasRef = ref()
 const w = ref(CANVAS_WIDTH)
 const h = ref(CANVAS_HEIGHT)
 const status = ref<'start' | 'pause' | 'restart'>()
+
+let player1Score = ref(0)
+let player2Score = ref(0)
 
 const closeWindow = () => {
   window.opener = null
@@ -22,7 +26,12 @@ const closeWindow = () => {
 
 const startGame = () => {
   status.value = 'start'
-  stage.start()
+  loadResource().then(() => {
+    const canvas = canvasRef.value as HTMLCanvasElement
+    const ctx = canvas!.getContext('2d')
+    stage = new Stage(img, ctx!, w.value, h.value)
+    stage.start()
+  })
 }
 
 const pauseGame = () => {
@@ -35,17 +44,30 @@ const restartGame = () => {
   stage.resume()
 }
 
+const loadResource = () => {
+  return new Promise((resolve) => {
+    img = new Image()
+    img.src = graghics
+    img.onload = () => {
+      resolve(1)
+    }
+  })
+}
+
+const player1Style = {
+  backgroundImage: `url(${graghics})`,
+  backgroundPosition: '-803px -3px',
+}
+
+const player2Style = {
+  backgroundImage: `url(${graghics})`,
+  backgroundPosition: '-675px -3px',
+}
+
 onMounted(() => {
   window.$eventBus = new EventBus()
   window.$CountDownGen = CountDown.gen
   window.$CountDownGen2 = CountDown.gen2
-  const img = new Image()
-  img.src = graghics
-  img.onload = () => {
-    const canvas = canvasRef.value as HTMLCanvasElement
-    const ctx = canvas!.getContext('2d')
-    stage = new Stage(img, ctx!, w.value, h.value)
-  }
   window.$eventBus.on({
     eventName: 'gameover',
     func: () => {
@@ -69,12 +91,25 @@ onMounted(() => {
 
 <template>
   <div absolute top-16px left-0 right-0 m-auto flex justify-center items-center h-60px>
-    <el-button v-if="!status" type="primary" @click="startGame">开始游戏</el-button>
     <el-button v-if="status === 'start'" type="primary" @click="pauseGame">暂停游戏</el-button>
     <el-button v-if="status === 'pause'" type="primary" @click="restartGame">恢复游戏</el-button>
   </div>
-  <div w-full h-full flex justify-center items-center>
-    <canvas ref="canvasRef" width="800" height="600" class="bg-black"></canvas>
+  <div w-full h-full flex justify-center items-center v-if="status">
+    <canvas ref="canvasRef" width="800" height="600" class="bg-black mr-10px"></canvas>
+    <div class="flex-1 grid grid-cols-[1fr_3fr] grid-rows-[60px_60px]">
+      <span class="font-semibold">玩家类别</span><span class="font-semibold">歼敌数量</span>
+      <div class="flex">
+        <span class="w-26px h-26px mr-4px" :style="player1Style"></span><span>(玩家一)</span>
+      </div>
+      <span>{{ player1Score }}</span>
+      <div class="flex">
+        <span class="w-26px h-26px mr-4px" :style="player2Style"></span><span>(玩家二)</span>
+      </div>
+      <span>{{ player2Score }}</span>
+    </div>
+  </div>
+  <div v-else class="w-full h-full flex justify-center items-center">
+    <el-button v-if="!status" type="primary" @click="startGame">开始游戏</el-button>
   </div>
 </template>
 
